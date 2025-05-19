b06253c57d268113c2d9da0893165bca54f3700a
William Rutkowski <budski82@users.noreply.github.com>
2025-05-19 11:52:09 -0400

    import project starter files

---
+++ js/_classes/Player.js
@@ -0,0 +1,10 @@
+export default class Player{
+    constructor(game){
+
+    }
+}
+
+
+
+console.log('player' + '' + 12345);
+export {Player}
---
+++ js/entry.js
@@ -0,0 +1,14 @@
+import {Player} from './_classes/Player.js'
+
+
+addEventListener('load', function(){
+    const canvas = document.getElementById('cvs')
+    const ctx    = canvas.getContext('2d')
+
+    let ctxH = canvas.height = 500
+    let ctxW = canvas.width  = 500 
+
+})
+
+
+console.log(12534);
